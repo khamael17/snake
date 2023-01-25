832c81b746933017b0094a0cc67664329d570386
Khalilou <khamael17@gmail.com>
2023-01-25 17:18:46 -0500

    first commit

---
+++ src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        GameFrame frame= new GameFrame();
+
+
+        System.out.println("Hello, World!");
+    }
+}
